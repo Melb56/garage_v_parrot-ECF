c258fe326608b69956e270843c5c3d07aa39150f
Melanie <melanie.bruzac@outlook.fr>
2023-07-11 11:36:04 +0200

    Added h1 tag

--- migrations/Version20230711093208.php
+++ migrations/Version20230711093208.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230710143112 extends AbstractMigration
+final class Version20230711093208 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,28 +20,28 @@ final class Version20230710143112 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `admin` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_880E0D76E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE contact_form (id INT AUTO_INCREMENT NOT NULL, lastname VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, object TINYINT(1) NOT NULL, message_content LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_5D9F75A1E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE filters (id INT AUTO_INCREMENT NOT NULL, product_id INT DEFAULT NULL, price NUMERIC(10, 0) NOT NULL, km NUMERIC(10, 0) NOT NULL, date_time NUMERIC(10, 0) NOT NULL, INDEX IDX_7877678D4584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE opinion (id INT AUTO_INCREMENT NOT NULL, fistname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, date_time NUMERIC(10, 0) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE filters ADD product_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, title VARCHAR(255) NOT NULL, price NUMERIC(10, 0) NOT NULL, date_time NUMERIC(10, 0) NOT NULL, km NUMERIC(10, 0) NOT NULL, description LONGTEXT NOT NULL, INDEX IDX_D34A04ADA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE filters ADD CONSTRAINT FK_7877678D4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
-        $this->addSql('CREATE INDEX IDX_7877678D4584665A ON filters (product_id)');
-        $this->addSql('ALTER TABLE product ADD user_id INT NOT NULL');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADA76ED395 FOREIGN KEY (user_id) REFERENCES employee (id)');
-        $this->addSql('CREATE INDEX IDX_D34A04ADA76ED395 ON product (user_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE filters DROP FOREIGN KEY FK_7877678D4584665A');
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04ADA76ED395');
+        $this->addSql('DROP TABLE `admin`');
         $this->addSql('DROP TABLE contact_form');
         $this->addSql('DROP TABLE employee');
+        $this->addSql('DROP TABLE filters');
         $this->addSql('DROP TABLE opinion');
-        $this->addSql('DROP INDEX IDX_D34A04ADA76ED395 ON product');
-        $this->addSql('ALTER TABLE product DROP user_id');
-        $this->addSql('ALTER TABLE filters DROP FOREIGN KEY FK_7877678D4584665A');
-        $this->addSql('DROP INDEX IDX_7877678D4584665A ON filters');
-        $this->addSql('ALTER TABLE filters DROP product_id');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
