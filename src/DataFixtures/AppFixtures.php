98d4615c5488a8586b1563590d6bf1d37b2c3832
Melanie <melanie.bruzac@outlook.fr>
2023-07-14 11:49:20 +0200

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,18 +2,42 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Employee;
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
-    {
-        #$product = new Product();
-        #$product = setName()
-            #->setPrice()
+    {   
+        // Utilisation de Faker
+        $faker = Factory::create('fr_FR');
+        
+        // Creation d'un user
+        $employee = new employee();
 
-        #$manager->persist($object);
+        $employee->setEmail('user@test.com')
+                 ->setPassword($faker->password);
+
+                 $manager->persist($employee);
+
+
+        // Creation d'un produit
+        for ($i=0; $i < 10; $i++) {
+            $product = new Product();
+
+            $product->setTitle($faker->words(2, true))
+                    ->setPrice($faker->randomFloat(2, 100, 9999))
+                    ->setdateTime($faker->date('2000', '2022'))
+                    ->setKm($faker->randomFloat(2, 100, 9999))
+                    ->setDescription($faker->text(500))
+                    ->setUser($employee);
+                    
+            $manager->persist($product);
+        }
+
         $manager->flush();
     }
 }
